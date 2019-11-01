4c8c92c4349bf9e87e198642b20200fafd914487
ARTEM GARYKAVIY <artem.garykaviy@thomascook.com>
2019-11-01 17:58:04 +0200

    if else switch homework

--- Exercises/if-else-switch/if-else-switch.js
+++ Exercises/if-else-switch/if-else-switch.js
@@ -6,9 +6,44 @@ const codeToMissingComplexityElement = {
 };
 
 function getMissingComplexityElementCode(password) {
-
+  if (!password) {
+    return codeToMissingComplexityElement[3];
+  }
+  if (password.length <= 8) {
+    return codeToMissingComplexityElement[0];
+  }
+  if (password === password.toLowerCase()) {
+    return codeToMissingComplexityElement[1];
+  }
+  if (!password.match(/.*[^A-Za-z0-9]/)) {
+    return codeToMissingComplexityElement[2];
+  }
+  return 'Good job!';
 }
 
-function getErrorMessage() {
-  return false;
+function getErrorMessage(getMissingComplexityElementCode) {
+  // eslint-disable-next-line default-case
+  switch (getMissingComplexityElementCode) {
+  case 'cannot be empty':
+    console.log('Password cannot be empty. Please enter a password.');
+    break;
+  case 'length':
+    console.log('Password should contain no less than 8 characters.');
+    break;
+  case 'capital letter':
+    console.log('Password should contain at least one capital letter.');
+    break;
+  case 'special character':
+    console.log('Password should contain at leas one special character.');
+    break;
+  default:
+    console.log('Password is strong!');
+  }
 }
+
+function enterPassword(password) {
+  console.log(getMissingComplexityElementCode(password));
+  getErrorMessage(getMissingComplexityElementCode(password));
+}
+
+enterPassword('');
